7f30cc8a6f6255d74bbef3bf6e5480f3b20b2d50
Henrique Melanda <hjdesigner23@gmail.com>
2019-02-24 11:56:52 -0300

    Created the function checks if a day has been selected to add appointment, when selected it checks if it already has an appointment that same day, if it shows a warning message and disables the button.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const btnAddAppointement = document.querySelector('[data-js="btnAppointebt"]');
 const formElementAdd = document.querySelector('[data-js="formAdd"]');
 const selectDay = document.querySelector('[data-js="selectDay"]');
+const helpAlert = document.querySelector('[data-js="helpAlert"]');
+const addForm = document.querySelector('[data-js="addForm"]');
 
 btnAddAppointement.addEventListener('click', () => {
   formElementAdd.classList.toggle('active');
 });
 
 selectDay.addEventListener('change', (e) => {
-	const appointementDay = document.querySelector(`${e.target.value}`);
-	console.log(appointementDay);
+	const appointementDay = document.querySelector(`[data-js="${e.target.value}"]`);
+	helpAlert.classList.remove('active');
+	addForm.disabled = false;
+	if (appointementDay.childNodes.length > 1) {
+		helpAlert.classList.add('active');
+		addForm.disabled = true;
+	}
 });
