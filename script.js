a3acbd1de088a564fb8c45bb4b542b07722b0169
Henrique Melanda <hjdesigner23@gmail.com>
2019-02-25 22:47:31 -0300

    Created event to validate if it is not trying to register an appointment without title, if you try to leave the field blank it will appear an error message to the user if everything is right it calls a function in which adds the title in the calendar on the day selected by the user .

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const btnAddAppointement = document.querySelector('[data-js="btnAppointebt"]');
 const formElementAdd = document.querySelector('[data-js="formAdd"]');
 const selectDay = document.querySelector('[data-js="selectDay"]');
 const helpAlert = document.querySelector('[data-js="helpAlert"]');
+const helpError = document.querySelector('[data-js="helpError"]');
 const addForm = document.querySelector('[data-js="addForm"]');
+const titleForm = document.querySelector('[data-js="titleForm"]');
+
+function htmlAppintementDay(day, title) {
+	const elementCalendar = document.querySelector(`[data-js="${day}"]`);
+	const elementHTML = `<div class="calendar__appointment_item"><h2>${title}</h2></div>`;
+	elementCalendar.innerHTML = elementHTML;
+}
 
 btnAddAppointement.addEventListener('click', () => {
   formElementAdd.classList.toggle('active');
@@ -10,10 +18,34 @@ btnAddAppointement.addEventListener('click', () => {
 
 selectDay.addEventListener('change', (e) => {
 	const appointementDay = document.querySelector(`[data-js="${e.target.value}"]`);
+	console.log(appointementDay.childNodes);
 	helpAlert.classList.remove('active');
 	addForm.disabled = false;
-	if (appointementDay.childNodes.length > 1) {
+	if (appointementDay.childNodes.length >= 1) {
 		helpAlert.classList.add('active');
 		addForm.disabled = true;
+	}
+});
+
+addForm.addEventListener('click', () => {
+	const valueTitle = document.querySelector('[data-js="titleForm"]');
+	if (valueTitle.value === '') {
+		helpError.classList.add('active');
+		return
+	}
+
+	let textTitle = valueTitle.value;
+	let textDay = document.querySelector('[data-js="selectDay"]').value;
+
+	valueTitle.value = '';
+	selectDay.selectedIndex = 0;
+	htmlAppintementDay(textDay, textTitle);
+});
+
+titleForm.addEventListener('blur', (e) => {
+	const value = e.target.value;
+	if (value.length >= 1) {
+		helpError.classList.remove('active');
+		return
 	}
 });
